06b9bcedcc264dfaccb25fa083f04397b21198a9
Shripad Rao <shripada0987@gmail.com>
2023-03-18 15:09:04 +0530

    get and display live location on map

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-// import 'package:latlng/latlng.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:safemap/utils/getLiveLocation.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,34 +16,105 @@ class MyApp extends StatelessWidget {
       title: 'My App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Map View'),
+          title: Text('SafeMap'),
         ),
-        body: MapView(),
+        body: MapScreen(),
       ),
     );
   }
 }
 
-class MapView extends StatelessWidget {
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late MapController _mapController = MapController();
+  late Position? _currentPosition = Position(
+      longitude: 77.59,
+      latitude: 12.97,
+      timestamp: DateTime.now(),
+      accuracy: 15.0,
+      altitude: 10.0,
+      heading: 0.0,
+      speed: 0.0,
+      speedAccuracy: 0.0);
+  // Position(longitude: 12.9716, latitude: 77.5946)
+
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+    determinePosition().then((position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    }).catchError((e) {
+      print('Error: $e');
+    });
+  }
+
+  void _centerMap() {
+    if (_currentPosition != null) {
+      _mapController.move(
+        LatLng(
+          _currentPosition!.latitude,
+          _currentPosition!.longitude,
+        ),
+        18.0,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(51.509364, -0.128928),
-        zoom: 19.2,
+    return Scaffold(
+      body: _currentPosition != null
+          ? FlutterMap(
+              mapController: _mapController,
+              options: MapOptions(
+                  center: LatLng(
+                    _currentPosition!.latitude,
+                    _currentPosition!.longitude,
+                  ),
+                  zoom: 18.0,
+                  maxZoom: 18.2),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(
+                        _currentPosition!.latitude,
+                        _currentPosition!.longitude,
+                      ),
+                      builder: (ctx) => Container(
+                        child: Icon(
+                          Icons.accessibility_new_sharp,
+                          size: 45.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _centerMap,
+        child: Icon(Icons.my_location),
       ),
-      nonRotatedChildren: [
-        AttributionWidget.defaultWidget(
-          source: 'OpenStreetMap contributors',
-          onSourceTapped: null,
-        ),
-      ],
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-      ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
---
+++ lib/utils/getLiveLocation.dart
@@ -0,0 +1,30 @@
+import 'package:geolocator/geolocator.dart';
+
+Future<Position> determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    throw ('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      throw ('Location permissions are denied');
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    throw ('Location permissions are permanently denied, we cannot request permissions.');
+  }
+  try {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return position;
+  } catch (e) {
+    print('Error: $e');
+    throw ('Could not get the location.');
+  }
+}
